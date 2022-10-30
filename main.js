074e3abe418cc7279d82dd6b6b57642f21b5a8c1
Faisal ahmed <faisal2410@yahoo.com>
2022-10-30 19:49:36 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
+console.log("We love javascript");
